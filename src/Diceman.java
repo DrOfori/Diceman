1fb4f8700f33b13b76579f17813dc4bbe8a11717
Namirah0310 <nare0001@stud.ek.dk>
2025-09-05 14:39:37 +0200

    Diceman01 creation of dice man constructor.

--- src/Diceman.java
+++ src/Diceman.java
@@ -1,4 +1,19 @@
 package PACKAGE_NAME;
 
 public class Diceman {
+    private String name;
+    private int numberOfActivities;
+
+    public Diceman(String name, int numberOfActivities) {
+        this.name = name;
+        this.numberOfActivities = numberOfActivities;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfActivities() {
+        return numberOfActivities;
+    }
 }
