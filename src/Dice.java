cf2704c0379094d73a16cb7186614b3728e326e0
DrOfori <simonbelai045@gmail.com>
2025-09-05 15:06:31 +0200

    rolldice

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Dice {
      int face1;
      int face2;
@@ -6,12 +8,20 @@ public class Dice {
      int face5;
      int face6;
 
-    public dice(int face1, int face2, int face3, int face4, int face5, int face6){
+    public int Dice(int face1, int face2, int face3, int face4, int face5, int face6){
         this.face1 = face1;
         this.face2 = face2;
         this.face3 = face3;
         this.face4 = face4;
         this.face5 = face5;
         this.face6 = face6;
+
+        Random rand = new Random();
+        int roll = rand.nextInt(6)+1;
+
+        public int rolldice(); {
+            return roll;
+        }
+        System.out.println("you rolled a " + roll);
     }
 }
