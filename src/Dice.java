db97bdb03099c97f04b346f34dae6c1369cd2599
DrOfori <simonbelai045@gmail.com>
2025-09-05 15:14:38 +0200

    fix

--- src/Dice.java
+++ src/Dice.java
@@ -8,20 +8,19 @@ public class Dice {
      int face5;
      int face6;
 
-    public int Dice(int face1, int face2, int face3, int face4, int face5, int face6){
+    public int Dice(int face1, int face2, int face3, int face4, int face5, int face6) {
         this.face1 = face1;
         this.face2 = face2;
         this.face3 = face3;
         this.face4 = face4;
         this.face5 = face5;
         this.face6 = face6;
+    }
 
+       public int rolldice() {
         Random rand = new Random();
-        int roll = rand.nextInt(6)+1;
-
-        public int rolldice(); {
+        int roll = rand.nextInt(6) + 1;
             return roll;
         }
-        System.out.println("you rolled a " + roll);
-    }
+
 }
