5442b689ddc4aa0e154838bbd0aad9e02a7a99fc
DrOfori <simonbelai045@gmail.com>
2025-09-05 14:42:32 +0200

    Dice constructor

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Dice {
+     int face1;
+     int face2;
+     int face3;
+     int face4;
+     int face5;
+     int face6;
+
+    public dice(int face1, int face2, int face3, int face4, int face5, int face6){
+        this.face1 = face1;
+        this.face2 = face2;
+        this.face3 = face3;
+        this.face4 = face4;
+        this.face5 = face5;
+        this.face6 = face6;
+    }
 }
