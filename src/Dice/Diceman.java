e3f3165e0838a45aed13c815a2a01fc950650353
Namirah0310 <nare0001@stud.ek.dk>
2025-09-05 15:31:23 +0200

    Diceman03 moved diceman to package.

---
+++ src/Dice/Diceman.java
@@ -0,0 +1,26 @@
+package PACKAGE_NAME.Dice;
+
+public class Diceman {
+    private String name;
+    private int numberOfActivities;
+
+    public Diceman(String name, int numberOfActivities) {
+        this.name = name;
+        this.numberOfActivities = numberOfActivities;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfActivities() {
+        return numberOfActivities;
+    }
+    public void doAction() {
+        while (numberOfActivities <= 5) {
+
+        }
+    }
+
+}
+
