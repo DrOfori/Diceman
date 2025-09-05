7e3c1774e12136d5c71ccd0373dc604c2043e656
DrOfori <simonbelai045@gmail.com>
2025-09-05 15:19:56 +0200

    -int

--- src/Dice.java
+++ src/Dice.java
@@ -8,7 +8,7 @@ public class Dice {
      int face5;
      int face6;
 
-    public int Dice(int face1, int face2, int face3, int face4, int face5, int face6) {
+    public Dice(int face1, int face2, int face3, int face4, int face5, int face6) {
         this.face1 = face1;
         this.face2 = face2;
         this.face3 = face3;
